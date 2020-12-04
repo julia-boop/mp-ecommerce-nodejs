79e35c4c78f010682a78082c09b8d6d33ee8534c
julia-boot <juliacordero@MacBook-Pro.local>
2020-12-04 18:24:50 -0300

    app.use

--- app.js
+++ app.js
@@ -13,9 +13,13 @@ var app = express();
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use(express.static('assets'));
  
 app.use('/assets', express.static(__dirname + '/assets'));
+
 
 app.get('/', function (req, res) {
     res.render('home');
